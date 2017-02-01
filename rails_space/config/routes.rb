9d97a9c031ccff687f8167cab95d2b4e11b6e51d
SATYA ADITYA PRANEETH EMANI <prasaadem@gmail.com>
2017-02-01 13:20:06 -0600

    First Index Page

--- rails_space/config/routes.rb
+++ rails_space/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'site/index'
+
+  get 'site/about'
+
+  get 'site/help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
